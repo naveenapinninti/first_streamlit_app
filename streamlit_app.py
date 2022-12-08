46ce18b890ecd6a16ce5f9266b7f5f2484bfef97
naveenapinninti <118549997+naveenapinninti@users.noreply.github.com>
2022-12-08 21:46:31 +0530

    Update streamlit_app.py
    added one header
    added three text messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3& blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlt.text('Hard-Bolied Free range egg') 
